757e1cd4e5b3c059d5f28e47ddc47663ee7926ae
Muthu Nexus Digitalia <karthiganexusdigitalia@gmail.com>
2024-09-10 15:55:54 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,280 @@
+  // carousel 
+  document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = document.querySelector('#carouselExampleAutoplaying');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 1500, 
+        ride: 'carousel'
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel = document.querySelector('#carouselExampleAutoplaying1');
+  var carousel1 = new bootstrap.Carousel(myCarousel, {
+      interval: 1500, 
+      ride: 'carousel1'
+  });
+});
+
+// trigger
+
+document.getElementById("whatsappButton").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "Hi there, here is the blue horizon ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton1").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "Are you Looking for your dream home? We're here to help you find the perfect place.";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton2").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center and was wondering if you know anything about it.Do you have any details or tips on how to get started? I’d really appreciate any info you can share! ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+
+document.getElementById("whatsappButton3").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining CBSC & ICSC class";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton4").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center Separate Subject Classes";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton5").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center Home Tuition ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton6").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center Online Class ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton7").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center and was wondering if you know anything about it. Do you have any details or tips on how to get started? I’d really appreciate any info you can share! ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton8").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center and was wondering if you know anything about it. Do you have any details or tips on how to get started? I’d really appreciate any info you can share! ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+document.getElementById("whatsappButton9").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I’m thinking of joining S.V.G Tuition Center and was wondering if you know anything about it. Do you have any details or tips on how to get started? I’d really appreciate any info you can share! ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton10").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I Want to buy Plots ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+document.getElementById("whatsappButton11").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I Want to buy Plots";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+document.getElementById("whatsappButton12").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "I Want to buy Plots";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton13").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = "Welcome to S.V.G.Home Tuition";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+document.getElementById("whatsappButton14").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = " I'am Interest For This Job ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+document.getElementById("whatsappButton15").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = " I’m thinking of joining S.V.G Tuition Center and was wondering if you know anything about it. Do you have any details or tips on how to get started? I’d really appreciate any info you can share!  ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+document.getElementById("whatsappButton16").addEventListener("click", function () {
+  var phoneNumber = "+918778019356"; 
+  var message = " I Want to buy Plots ";
+  window.open(
+    "https://api.whatsapp.com/send?phone=" +
+      phoneNumber +
+      "&text=" +
+      encodeURIComponent(message),
+    "_blank"
+  );
+});
+
+
+
+// top button
+
+
+const button2 = document.getElementById("button1");
+
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 100) {
+      button2.style.display = "block";
+    } else {
+      button2.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button2.addEventListener("click", (event) => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
+
+
+
+// navbar
+document.addEventListener('DOMContentLoaded', function () {
+  var navbarToggler = document.querySelector('.navbar-toggler');
+  var navbarCollapse = document.querySelector('#navbarCollapse');
+  
+  document.querySelectorAll('#navbarCollapse .nav-link').forEach(function (link) {
+      link.addEventListener('click', function () {
+          if (window.getComputedStyle(navbarCollapse).display !== 'none') {
+              navbarToggler.click();
+          }
+      });
+  });
+});
+
